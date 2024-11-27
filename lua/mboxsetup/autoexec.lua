29a7793babeeb4bb79695b1990dfd09c07e42c05
KyleIsDork <53096073+KyleIsDork@users.noreply.github.com>
2024-11-27 15:27:48 -0500

    Begin mboxsetup folder, begin autoexec.lua

---
+++ lua/mboxsetup/autoexec.lua
@@ -0,0 +1,11 @@
+-- from forums, somewhere
+--autoexec.lua
+--place in C:\%userprofile%\AppData\Local
+--local path = os.getenv('localappdata')
+--LoadScript(path .. [[\ammo.lua ]])
+
+LoadScript("C:\\Users\\<WINDOWS USERNAME>\\AppData\\Local\\multiboxhelper.lua")
+LoadScript("C:\\Users\\<WINDOWS USERNAME>\\AppData\\Local\\vaccibucks.lua")
+LoadScript("C:\\Users\\<WINDOWS USERNAME>\\AppData\\Local\\sniperbuybot.lua")
+UnloadScript("C:\\Users\\<WINDOWS USERNAME>\\AppData\\Local\\vaccibucks.lua")
+UnloadScript("C:\\Users\\<WINDOWS USERNAME>\\AppData\\Local\\sniperbuybot.lua")
